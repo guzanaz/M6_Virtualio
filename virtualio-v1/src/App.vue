a55afb8b7ca148fbff54227691c2ec3a80caf88a
Daniela <daniela.gallardo.reyes@gmail.com>
2021-11-25 11:34:54 +0100

    last update
    before presentation

--- virtualio-v1/src/App.vue
+++ virtualio-v1/src/App.vue
@@ -1,34 +1,33 @@
 <template>
-<div>
-  <Navbar/>
-  <LoginSection/>
-  <SobreVirtualio/>
-  <GoogleMap/>
-  <ContactForm/>
-   <Footer/>
-   </div>
+  <div>
+    <Navbar />
+    <LoginSection />
+    <SobreVirtualio />
+    <GoogleMap />
+    <ContactForm />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import LoginSection from './components/LoginSection.vue'
-import SobreVirtualio from './components/SobreVirtualio.vue'
-import GoogleMap from './components/GoogleMap.vue'
-import ContactForm from './components/ContactForm.vue'
-import Footer from './components/Footer.vue'
+import Navbar from "./components/Navbar.vue";
+import LoginSection from "./components/LoginSection.vue";
+import SobreVirtualio from "./components/SobreVirtualio.vue";
+import GoogleMap from "./components/GoogleMap.vue";
+import ContactForm from "./components/ContactForm.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
     LoginSection,
     SobreVirtualio,
     GoogleMap,
     ContactForm,
-    Footer
-
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style>
@@ -36,7 +35,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
- 
 }
 </style>
