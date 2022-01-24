789c1388b4b368d3e8e6a945a0db91f76d2daed5
Daniela <daniela.gallardo.reyes@gmail.com>
2022-01-24 18:56:15 +0100

    It runs!
    the project starts after uninstalling bootstrap and bootstrap icons

--- virtualio-v1/src/main.js
+++ virtualio-v1/src/main.js
@@ -1,15 +1,15 @@
-import "bootstrap/dist/css/bootstrap.css"
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js' // <---
 import store from './store'
-import "bootstrap-icons/font/bootstrap-icons.css"
+
 
 
 
 
 createApp(App).use(router).use(store).mount('#app')//<-- createApp(App).mount('#app')
-import "bootstrap/dist/js/bootstrap.js"
+
 
 
 
