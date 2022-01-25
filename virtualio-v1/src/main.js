a7aebb89be05f36f05a303580c58a10d583c5b12
Daniela <daniela.gallardo.reyes@gmail.com>
2022-01-25 19:39:11 +0100

    kk

--- virtualio-v1/src/main.js
+++ virtualio-v1/src/main.js
@@ -1,10 +1,14 @@
-
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js' // <---
 import store from './store'
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+import "bootstrap-icons/font/bootstrap-icons.css"
+>>>>>>> parent of 789c138 (It runs!)
 
 
 
@@ -12,7 +16,7 @@ import store from './store'
 >>>>>>> parent of 9dadc28 (Bootstrap icon libary sucessfully integrated)
 
 createApp(App).use(router).use(store).mount('#app')//<-- createApp(App).mount('#app')
-
+import "bootstrap/dist/js/bootstrap.js"
 
 
 
